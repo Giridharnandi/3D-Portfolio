281fc353f6b2261cbdd64892798622a92daa4189
Glop-org <glop.reflect@gmail.com>
2024-10-24 11:56:38 +0530

    adding commit changes

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Fullstack Web Developer 
+          </h1>
+        </motion.div>
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Saas Product Developer 
+          </h1>
+        </motion.div>
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Founder GlopLink
+          </h1>
+        </motion.div>
+        
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+            project exprience
+          </span>
+        </motion.div>
+        <p className="font-semibold">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              By Giridhar Nandi{" "}
+            </span>
+        </p>
+
+        <motion.p
+  variants={slideInFromLeft(0.8)}
+  className="text-lg text-gray-400 my-5 max-w-[600px] leading-relaxed text-justify mx-auto px-4 md:px-0"
+>
+  I&apos;m a <b className="text-white">Full Stack Web Developer</b> and <b className="text-white">SaaS Product Developer</b>, with extensive experience in building websites, mobile applications, 
+  and software solutions. Currently, I am focused on developing my own SaaS business startup, leveraging cutting-edge technologies to create scalable
+  and efficient platforms. Explore my portfolio to see a range of projects that highlight my skills in both frontend and backend development, product design,
+  and cloud infrastructure management.
+</motion.p>
+
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
